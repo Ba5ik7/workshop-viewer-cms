44709023f498b1edd922c0b2c5c575333e099187
Wesley DuSell <dusellfam@gmail.com>
2022-07-01 10:40:02 -0400

    Setup POC routes to display workshops

--- src/app/pages/workshop-editor/workshop-editor-routing.module.ts
+++ src/app/pages/workshop-editor/workshop-editor-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkshopEditorComponent } from './workshop-editor.component';
 
-const routes: Routes = [{ path: '', component: WorkshopEditorComponent }];
+const routes: Routes = [
+  {
+    path: ':categoryId',
+    component: WorkshopEditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
