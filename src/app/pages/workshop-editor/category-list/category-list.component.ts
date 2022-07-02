4dfbf9bbe8b71617ed91337353764ea902194ead
Wesley DuSell <dusellfam@gmail.com>
2022-07-02 00:03:35 -0400

    pass down categories

--- src/app/pages/workshop-editor/category-list/category-list.component.ts
+++ src/app/pages/workshop-editor/category-list/category-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 })
 export class CategoryListComponent implements OnInit {
 
-  @Input() categories!: any[];
+  @Input() categories!: any[] | null;
 
   constructor() {
   }
