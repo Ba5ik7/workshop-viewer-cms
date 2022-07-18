3d81bda377951e4b9161eb292a26c16751e239d7
Wesley DuSell <dusellfam@gmail.com>
2022-07-17 22:08:41 -0400

    Playing around with quill styles

--- src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
@@ -20,6 +20,7 @@ import { LiveExampleComponent } from './live-example/live-example.component';
 import { NextPageComponent } from './next-page/next-page.component';
 import { WorkshopViewerService } from './workshop-viewer.service';
 
+
 @Component({
   selector: 'workshop-viewer-terrence-dusell',
   templateUrl: './workshop-viewer.component.html',
@@ -27,6 +28,29 @@ import { WorkshopViewerService } from './workshop-viewer.service';
   encapsulation: ViewEncapsulation.None
 })
 export class WorkshopViewerComponent implements OnInit, OnDestroy {
+
+  modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+  
+      // [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      // [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      // [{ 'direction': 'rtl' }],                         // text direction
+  
+      // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  
+      ['clean'],                                         // remove formatting button
+  
+      // ['link', 'image', 'video']                         // link and image, video
+      ['link', 'image']                         // link and image, video
+    ]
+  };
 
   currentDocument!: string
 
@@ -85,7 +109,7 @@ export class WorkshopViewerComponent implements OnInit, OnDestroy {
     .pipe(takeUntil(this.destory))
     .subscribe((data) => {
       this.correctUrlPaths(data);
-      this.elementRef.nativeElement.innerHTML = data.html;
+      // this.elementRef.nativeElement.innerHTML = data.html;
       this.loadLiveExamples('workshop-live-example', LiveExampleComponent);
       this.loadCodeHighlighter('code-highlighter', CodeHighlighterComponent);
       this.loadNextPage();
