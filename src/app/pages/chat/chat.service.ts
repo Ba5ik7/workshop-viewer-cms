2b51a783c7b3b8dc05b67edd6a63704c7a71f951
Wesley DuSell <dusellfam@gmail.com>
2022-09-03 12:27:40 -0600

    Create interfaces for the objects sent to a from the server

--- src/app/pages/chat/chat.service.ts
+++ src/app/pages/chat/chat.service.ts
@@ -1,5 +1,24 @@
 import { Injectable } from '@angular/core';
 
+
+export interface Message {
+  user: string;
+  content: string;
+}
+
+export interface ChatRoom {
+  users: string[];
+  messages: Message[];
+}
+
+export interface ChatAppData {
+  activeRoom: string;
+  chatRoom: ChatRoom;
+  connected: boolean;
+  rooms: string[];
+  user: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
