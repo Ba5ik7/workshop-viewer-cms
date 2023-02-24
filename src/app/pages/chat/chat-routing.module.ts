77ef11c95ff4f796a4cbfe6b65b36471a4082e02
Wesley DuSell <dusellfam@gmail.com>
2023-02-23 20:48:30 -0500

    Add routing to the chat page

--- src/app/pages/chat/chat-routing.module.ts
+++ src/app/pages/chat/chat-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 import { ChatComponent } from './chat.component';
 
-const routes: Routes = [{ path: '', component: ChatComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'General'
+  },
+  {
+    canActivate: [AuthGuard],
+    path: ':categoryId',
+    component: ChatComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
