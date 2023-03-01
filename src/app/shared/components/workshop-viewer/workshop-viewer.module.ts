19bcbc30c5218b24cd38a80d6098f5edf38052c0
Wesley DuSell <dusellfam@gmail.com>
2023-02-28 22:15:41 -0500

    Stable build after upgrade

--- src/app/shared/components/workshop-viewer/workshop-viewer.module.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkshopViewerComponent } from './workshop-viewer.component';
 import { WorkshopViewerService } from './workshop-viewer.service';
-import { LiveExampleComponent } from './live-example/live-example.component';
+// import { LiveExampleComponent } from './live-example/live-example.component';
 import { CodeHighlighterModule } from '../code-highlighter/code-highlighter.module';
-import { NextPageComponent } from './next-page/next-page.component';
+// import { NextPageComponent } from './next-page/next-page.component';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxEditorjsModule } from '@tmdjr/ngx-editorjs';
 
@@ -12,8 +12,8 @@ import { NgxEditorjsModule } from '@tmdjr/ngx-editorjs';
 @NgModule({
   declarations: [
     WorkshopViewerComponent,
-    LiveExampleComponent,
-    NextPageComponent
+    // LiveExampleComponent,
+    // NextPageComponent
   ],
   exports: [WorkshopViewerComponent],
   imports: [
